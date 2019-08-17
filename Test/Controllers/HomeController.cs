4cfa9cc7a9f7a9704951bf398b668e09cdc9d9ef
Furynoobas <samsung12345#>
2019-08-17 17:05:15 +0300

    Add project files.

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+using NLog.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        [Filter.Filter]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            //logger.SetProperty("action", null);
+            //logger.SetProperty("Controller", null);
+            //logger.SetProperty("CorrealactionId", null);
+            //logger.SetProperty("UserId", null);
+            //logger.Info(Environment.NewLine + DateTime.Now);
+           
+
+            return View();
+        }
+    }
+}
---
+++ Test/Filter/Filter.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace Test.Filter
+{
+    public class Filter : ActionFilterAttribute
+    {
+        
+          private static Logger logger = LogManager.GetCurrentClassLogger();
+          public override void OnActionExecuting(ActionExecutingContext actionContext)
+         {
+            var user = (actionContext.Controller as Controller)?.User as ClaimsPrincipal;
+            var id = user.Claims.FirstOrDefault(i => i.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+            if (id != null)
+
+                // logger.SetProperty("UserId",);
+                logger.SetProperty("controller", actionContext.ActionDescriptor.ControllerDescriptor.ControllerName);
+            logger.SetProperty("Action", actionContext.ActionDescriptor.ActionName);
+            logger.Info(Environment.NewLine + DateTime.Now);
+            logger.SetProperty("CorrealactionId", null);
+            logger.SetProperty("UserId", id);
+        }
+    
+    }
+}
